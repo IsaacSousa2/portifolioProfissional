cad13fd92acd98b2166d2cb7fc0b9b2425cbad3a
Cicero Isaac <131274652+IsaacSousa2@users.noreply.github.com>
2024-02-16 14:50:49 -0300

    Criando backdrop e organizando header

--- portfolio-app/my-app/src/components/headerComponents/Header.tsx
+++ portfolio-app/my-app/src/components/headerComponents/Header.tsx
@@ -1,27 +1,71 @@
-import { LinkHeader } from "./Link"
+'use client'
+
+import { Link } from "./Link"
+import { LuMenu } from "react-icons/lu";
+import { IoIosCloseCircleOutline } from "react-icons/io";
 import Image from "next/image"
 import Logo from "@/../../public/Images/HeaderImages/Logo.png"
+import { useState } from "react";
 
 export const Header = () => {
+
+    const [ openMenu, setOpenMenu ] = useState(false)
+
+    function abrirMenu() {
+
+        openMenu ? setOpenMenu(false) : setOpenMenu(true);
+
+    }
 
     return(
 
         <header>
 
-            <div className="w-full ">
+            <div className="w-full h-14">
 
-                <div className="">
+                <div className="flex  justify-center items-center">
 
-                    <div className="absolute">
-                        <Image width={100} height={100} src={Logo} alt="Logo" />
+                    <div className={`flex absolute right-2 md:top-1`}>
+                        <Image src={Logo} alt="Logo" height={90} width={90} />
                     </div>
 
-                    <div className="flex items-center justify-center gap-x-4">
-                        <LinkHeader texto="Sobre" link={"/"} />
-                        <LinkHeader texto="Conhecimentos" link={"/"} />
-                        <LinkHeader texto="Projetos" link={"/"} />
-                        <LinkHeader texto="Contato" link={"/"} />
-                        <LinkHeader texto="Experiência" link={"/"} />
+                    <div className="">
+
+                        <div onClick={abrirMenu} className={`text-white hidden md:flex absolute z-50 left-2 top-4 text-2xl`}>
+                            <LuMenu className={`${openMenu? 'hidden' : 'flex'}`} />
+                            <IoIosCloseCircleOutline className={`${openMenu? 'relative ml-28 flex' : 'hidden'}`} />
+                        </div>
+
+                        {openMenu? <div className="w-screen h-screen bg-black/20 fixed"></div> : <></>}
+
+                        <div className={`flex gap-x-4 md:flex-col md:bg-darkBlue md:h-screen md:gap-y-3 md:w-64 md:justify-center ${openMenu? 'md:absolute md:left-0':'duration-300 transition-all md:ml-[-12000px]'}`}>
+
+                            <div>
+                                <Link texto="Sobre" link={"#/"} />
+                            </div>
+                        
+                            <div>
+                                <Link texto="Conhecimentos" link={"#/"} />
+                            </div>
+
+                            <div>
+                                <Link texto="Projetos" link={"#/"} />
+                            </div>
+
+                            <div>
+                                <Link texto="Contato" link={"#/"} />
+                            </div>
+
+                            <div>
+                                <Link texto="Experiência" link={"#/"} />
+                            </div>
+
+                            <div className={`flex absolute left-20 md:bottom-1 ${openMenu? 'flex' : 'hidden'}`}>
+                                <Image src={Logo} alt="Logo" height={90} width={90} />
+                            </div>
+
+                        </div>
+
                     </div>
 
                 </div>
--- portfolio-app/my-app/src/components/headerComponents/Link.tsx
+++ portfolio-app/my-app/src/components/headerComponents/Link.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link'
 import { RiLink } from "react-icons/ri"; 
 
 type Props = {
@@ -8,13 +7,13 @@ type Props = {
 
 }
 
-export const LinkHeader = ({link, texto}: Props) => {
+export const Link = ({link, texto}: Props) => {
 
     return(
 
         <div className="flex items-center justify-center text-center cursor-pointer py-3 opacity-65 hover:opacity-100 transition-all duration-[0.4s]">
             <RiLink className='text-lightBlue text-[21px]'/>
-            <h2 className='text-white text-[14px] p-1'><a href={`/${link}`}>{texto}</a></h2>
+            <h2 className='text-white text-[14px] p-1'><a href={`${link}`}>{texto}</a></h2>
         </div>
 
     )
